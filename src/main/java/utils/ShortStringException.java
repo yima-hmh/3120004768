05308d4d8982f159d16eb3e1f142b9f98ea8cd4b
yima <1716645823@qq.com>
2022-09-19 17:38:01 +0800

    添加了短字符串异常类

---
+++ src/main/java/utils/ShortStringException.java
@@ -0,0 +1,19 @@
+package utils;
+
+public class ShortStringException extends Exception {
+    public ShortStringException() {
+        super();
+    }
+
+    public ShortStringException(String message) {
+        super(message);
+    }
+
+    public ShortStringException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ShortStringException(Throwable cause) {
+        super(cause);
+    }
+}
